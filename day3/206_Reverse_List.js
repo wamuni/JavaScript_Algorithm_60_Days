45d2a690ea33c983b754932700a843004f49c279
wamuni <boshi.pan@outlook.com>
2024-01-14 01:31:00 +0000

    two pointers solution for reversing linked list

---
+++ day3/206_Reverse_List.js
@@ -0,0 +1,12 @@
+const reverseList = (head) => {
+    if (!head || !head.next) return head;
+    let temp = null,
+        pre = null,
+        cur = head;
+    while (cur) {
+        temp = cur.next;
+        cur.next = pre;
+        pre = cur;
+        cur = temp;
+    }
+};
